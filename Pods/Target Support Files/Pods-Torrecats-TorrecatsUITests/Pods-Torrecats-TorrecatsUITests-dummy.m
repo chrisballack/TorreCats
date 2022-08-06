7d5746314dfbbb58db349b4ba1ab6042b2198eb7
Christians Bonilla <Christians.bonilla@fletx.co>
2022-08-06 11:20:21 -0500

    Initial Commit Pod Init Pod Install

---
+++ Pods/Target Support Files/Pods-Torrecats-TorrecatsUITests/Pods-Torrecats-TorrecatsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Torrecats_TorrecatsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Torrecats_TorrecatsUITests
+@end
---
+++ Torrecats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Torrecats
+//
+//  Created by Dev IOS on 6/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
